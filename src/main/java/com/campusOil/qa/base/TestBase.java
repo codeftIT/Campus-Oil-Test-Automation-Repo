9867de16c04e4e518d733dac2a4186d9e4b642af
Naresh <naresh@codeft.in>
2019-09-11 12:05:29 +0530

    commit

--- src/main/java/com/campusOil/qa/base/TestBase.java
+++ src/main/java/com/campusOil/qa/base/TestBase.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
+import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
@@ -19,6 +20,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.events.EventFiringWebDriver;
 
+import com.qa.coloroutput.com.Colorme;
 import com.qa.util.java.ExcelImporter;
 import com.qa.util.java.TestUtil;
 
@@ -33,6 +35,7 @@ public class TestBase {
 	private String browserType;
 	// public static String inputbrowser;
 	public static ExcelImporter ExcelData;
+	public static Colorme conhan;
 
 	public TestBase() {
 		try {
@@ -47,6 +50,7 @@ public class TestBase {
 		}
 	}
 
+	
 	/// Intialization...
 	public static void intialization() {
 		String browserName = prop.getProperty("browser");
@@ -63,6 +67,7 @@ public class TestBase {
 			driver = new FirefoxDriver();
 			EventFireCopy();
 		} else if (browserName.equals(("Mobile"))) {
+			
 			System.setProperty("webdriver.chrome.driver",
 					"C:\\Users\\dell\\Desktop\\Test Base files\\chromedriver_win32\\chromedriver.exe");
 			Map<String, String> mobileEmulation = new HashMap<String, String>();
@@ -82,7 +87,8 @@ public class TestBase {
 			 * case 12: Device = "Nexus 10"; 
 			 * case 13 :Device = "JioPhone 2";
 			 */
-			mobileEmulation.put("deviceName", "iPad");
+
+			mobileEmulation.put("deviceName", "iPhone X");
 
 			ChromeOptions chromeOptions = new ChromeOptions();
 			chromeOptions.setExperimentalOption("mobileEmulation", mobileEmulation);
@@ -128,4 +134,31 @@ public class TestBase {
 		driver.get(prop.getProperty("url"));
 	}
 
+	public static void MobileDevice() {
+		
+		String Device ;
+		int inputDevice;
+		Scanner UserInput = new Scanner(System.in);
+		
+		System.out.println(conhan.GREEN_BOLD_BRIGHT+"****Please Enter the Device option in the below list for Testing****\n1. Nexus 5\n2. Galaxy S5\n+"
+				+ "3. Pixel 2\n4. Pixel 2 XL\n5.iPhone 5/SE\n6. iPhone 6/7/8\n7. iPhone 6/7/8 Plus\n8.iPhone X\n9. iPad"
+				+ "\n10. iPad Pro\n11. Galaxy Note 3\n12. Nexus 10\n13. JioPhone 2"+conhan.RESET);
+		inputDevice = UserInput.nextInt();
+		
+		switch(inputDevice) {
+		 case 1 : Device = "Nexus 5"; 
+		 case 2 : Device ="Galaxy S5"; 
+		 case 3 : Device = "Pixel 2"; 
+		 case 4 : Device = "Pixel 2 XL";
+		 case 5 : Device = "iPhone 5/SE"; 
+		 case 6 : Device = "iPhone 6/7/8"; 
+		 case 7 : Device = "iPhone 6/7/8 Plus"; 
+		 case 8 : Device = "iPhone X"; 
+		 case 9 : Device = "iPad"; 
+		 case 10: Device = "iPad Pro"; 
+		 case 11: Device = "Galaxy Note 3"; 
+		 case 12: Device = "Nexus 10"; 
+		 case 13 :Device = "JioPhone 2";
+		}
+	}
 }
